b7db8222c06a91bf9a188e5196222c14c06cfd8a
Isaac Stumm <isaac.stumm@students.snow.edu>
2026-09-03 22:02:33 -0600

    I need to figure out the bubble sort, and I had to retype the array before each sort because the selection would sort it and then the bubble and insert would have best case every time

--- whichSortIsBest/Program.cs
+++ whichSortIsBest/Program.cs
@@ -1,10 +1,12 @@
-﻿public class Program()
+﻿using System.Threading.Channels;
+
+public class Program()
 {
     static void Main()
     {
         //Selection Sort
         int numbersAdded = 0;
-        int[] selectionArray = new int[5];
+        int[] masterArray = new int[5];
         int numberOfComparisons = 0;
         int numberOfSwaps = 0;
         int totalOperations = 0;
@@ -12,41 +14,133 @@
             {
                 Console.WriteLine("Input a number");
                 int number = int.Parse(Console.ReadLine());
-                selectionArray[numbersAdded] = number;
+                masterArray[numbersAdded] = number;
                 numbersAdded++;
             }
         //Display the array before sorting
         Console.WriteLine("Before sorting:");
-        for (int i = 0; i < selectionArray.Length; i++)
+        for (int i = 0; i < masterArray.Length; i++)
         {
-            Console.WriteLine(selectionArray[i]);
+            Console.WriteLine(masterArray[i]);
         }
         //Selection sort algorithm
-        for (int i = 0; i < selectionArray.Length - 1; i++)
+        for (int i = 0; i < masterArray.Length - 1; i++)
         {
             int minIndex = i;
-            numberOfComparisons++;
-            for (int j = i + 1; j < selectionArray.Length; j++)
+
+            for (int j = i + 1; j < masterArray.Length; j++)
             {
                 numberOfComparisons++;
-                if (selectionArray[j] < selectionArray[minIndex])
+
+                if (masterArray[j] < masterArray[minIndex])
                 {
                     minIndex = j;
                 }
-
             }
-            int temp = selectionArray[i];
-            selectionArray[i] = selectionArray[minIndex];
-            selectionArray[minIndex] = temp;
-            
+
+            if (minIndex != i)
+            {
+                numberOfSwaps++;
+
+                int temp = masterArray[i];
+                masterArray[i] = masterArray[minIndex];
+                masterArray[minIndex] = temp;
+            }
         }
         Console.WriteLine("After sorting:");
-        for (int i = 0; i < selectionArray.Length; i++)
+        for (int i = 0; i < masterArray.Length; i++)
         {
-            Console.WriteLine(selectionArray[i]);
+            Console.WriteLine(masterArray[i]);
         }
         totalOperations = numberOfComparisons + numberOfSwaps;
-        Console.WriteLine();
+        Console.WriteLine("Swaps: " + numberOfSwaps);
+        Console.WriteLine("Comparisons: " + numberOfComparisons);
+        Console.WriteLine("Total operations: " + totalOperations);
 
+        //Reset
+        numberOfComparisons = 0;
+        numberOfSwaps = 0;
+        totalOperations = 0;
+        int numbersAdded2 = 0;
+        //Remake the array for the next sort, though this is the best I've got for now
+        while (numbersAdded2 < 5)
+        {
+            Console.WriteLine("Input a number");
+            int number = int.Parse(Console.ReadLine());
+            masterArray[numbersAdded2] = number;
+            numbersAdded2++;
+        }
+
+
+        //Bubble Sort
+        for (int i = 0; i < masterArray.Length; i++)
+        {
+                numberOfComparisons++;
+            for(int j = 0; j < masterArray.Length - i -1; j++) {
+                if (masterArray[j] > masterArray[j + 1])
+                {
+                    int temp = masterArray[j];
+                    masterArray[j] = masterArray[j + 1];
+                    masterArray[j + 1] = temp;
+                    numberOfSwaps++;
+                }
+            }
+        }
+        Console.WriteLine("After bubble sorting:");
+        for (int i = 0; i < masterArray.Length; i++)
+        {
+            Console.WriteLine(masterArray[i]);
+        }
+        totalOperations = numberOfComparisons + numberOfSwaps;
+
+        Console.WriteLine("Comparisons: " + numberOfComparisons);
+        Console.WriteLine("Swaps: " + numberOfSwaps);
+        Console.WriteLine("Total operations: " + totalOperations);
+
+        numberOfComparisons = 0;
+        numberOfSwaps = 0;
+        totalOperations = 0;
+        int numbersAdded3 = 0;
+        while (numbersAdded3 < 5)
+        {
+            Console.WriteLine("Input a number");
+            int number = int.Parse(Console.ReadLine());
+            masterArray[numbersAdded3] = number;
+            numbersAdded3++;
+        }
+
+        //Insertion Sort
+        for (int i = 0; i < masterArray.Length; i++)
+        {
+            int temp = masterArray[i];
+            int j = i - 1;
+            while (j >= 0)
+            {
+                numberOfComparisons++;
+
+                if (temp < masterArray[j])
+                {
+                    masterArray[j + 1] = masterArray[j];
+                    numberOfSwaps++;
+                    j--;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            masterArray[j + 1] = temp;
+        }
+        Console.WriteLine("After insertion sorting:");
+        for (int i = 0; i < masterArray.Length; i++)
+        {
+            Console.WriteLine(masterArray[i]);
+        }
+        totalOperations = numberOfComparisons + numberOfSwaps;
+        Console.WriteLine("Comparisons: " + numberOfComparisons);
+        Console.WriteLine("Swaps: " + numberOfSwaps);
+        Console.WriteLine("Total operations: " + totalOperations);
     }
+
+
 }
